f390f86c88e432de457be7a6cc7714f5e4e6f27b
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2022-07-01 09:46:48 -0300

    initial commit

---
+++ src/test/kotlin/com/oriedroc/kotlinunittests/KotlinUnitTestsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oriedroc.kotlinunittests
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinUnitTestsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
